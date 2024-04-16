02bb35c0b98967aca67de2d5c09ca23b7c10e950
piek <piekvossen@gmail.com>
2024-04-16 14:16:03 +0200

    Update query.py
    Added Dutch

--- src/cltl/about/query.py
+++ src/cltl/about/query.py
@@ -20,51 +20,34 @@ class QnA:
         "What is the time?": lambda: strftime("It is currently %H:%M."),
         "day is it?": lambda: strftime("It is %A today."),
         "month is it?": lambda: strftime("It is %B today."),
-#         "How many friends?": lambda: "I have {} friends".format(len(os.listdir(config.PEOPLE_FRIENDS_ROOT))),
-#         "Who are your friends?": lambda: "My friends are {}. I like my friends!".format(
-#              ", ".join(name.replace(".bin", "") for name in os.listdir(config.PEOPLE_FRIENDS_ROOT))),
-#         "How many people did you meet?": lambda: "I met {} people today!".format(len(os.listdir(config.PEOPLE_NEW_ROOT)) - 1),
-#         "Who did you meet?": lambda: "I met {}!".format(
-#              ", ".join(name.replace(".bin", "") for name in os.listdir(config.PEOPLE_NEW_ROOT) if name != "NEW.bin")),
-        "Tell me a joke!": lambda: choice(JOKE),
-        "Tell a joke!": lambda: choice(JOKE),
-        "Do you know a joke?": lambda: choice(JOKE),
-        "Do you know any jokes?": lambda: choice(JOKE),
-        "joke!": lambda: choice(JOKE)
+        # "How many friends?": lambda: "I have {} friends".format(len(os.listdir(config.PEOPLE_FRIENDS_ROOT))),
+        # "Who are your friends?": lambda: "My friends are {}. I like my friends!".format(
+        #     ", ".join(name.replace(".bin", "") for name in os.listdir(config.PEOPLE_FRIENDS_ROOT))),
+        # "How many people did you meet?": lambda: "I met {} people today!".format(len(os.listdir(config.PEOPLE_NEW_ROOT)) - 1),
+        # "Who did you meet?": lambda: "I met {}!".format(
+        #     ", ".join(name.replace(".bin", "") for name in os.listdir(config.PEOPLE_NEW_ROOT) if name != "NEW.bin")),
+        "Tell me a joke!": lambda: choice(JOKE)
+    }
+
+    QNA_DYNAMIC_NL = {
+        "Met mij gaat alles": lambda: choice(HAPPY),
+        "Hoe laat is het?": lambda: strftime("Het is nu %H:%M."),
+        "Wat is de tijd?": lambda: strftime("Het is precies %H:%M."),
+        "dag is het?": lambda: strftime("Het is %A vandaag."),
+        "maand is het?": lambda: strftime("Het is %B ."),
+        "Vertel eens een grap!": lambda: choice(JOKE)
     }
     
-    QNA_DYNAMIC_NL = {
-        "Met mij gaat het": lambda: choice(HAPPY),
-        "Hoe laat is het?": lambda: strftime("Het is nu %H:%M."),
-        "Welke dag is het vandaag?": lambda: strftime("Het is %A vandaag."),
-        "Welke maand?": lambda: strftime("Het is nu %B."),
-        "Vertel me een grap!": lambda: choice(JOKE),
-        "Vertel me eens een mop grap!": lambda: choice(JOKE),
-        "Vertel een grap!": lambda: choice(JOKE),
-        "Ken je een grap?": lambda: choice(JOKE),
-        "Ken je een mop?": lambda: choice(JOKE),
-        "grap!": lambda: choice(JOKE)
-    }
-
     QNA_STATIC = {
-    	# CLTL master
-    	"tell me about the master": "If you want to learn more about Large Language Models, check out the Human Language Technology and Text Mining masters.",
-    	"master": "If you want to learn more about Large Language Models, check out the Human Language Technology and Text Mining masters.",    	
-    	"robot": "If you like me and want to know more, check out the Human Language Technology and Text Mining masters. The Research Master presentation on Language Technology is at 1pm in room KC-07 in the Medical Faculty and the Text Mining presentation is at 2pm in room 1A-43 in this building. Don't be late!",
-    	"llm": "Do you want to learn more about Large Language Models, check out the Human Language Technology master at 1pm in room KC-07 in the Medical Faculty and and Text Mining master at 2pm in room 1A-43 in this building. Don't be late!",
-    	"when is the presentation?": "The Human Language Technology presentation is at 2pm, the Text Mining presentation is at 1pm.  Don't be late!",
-    	"where is the presentation?": "The Human Language Technology presentation is in room KC-07 in the medical faculty across the campus,  and the Text Mining presentation is in room 1A43 in this building. Text Mining is on the first floor in the A-wing!",
-    	"when?": "The Human Language Technology presentation is at 2pm, the Text Mining presentation is at 1pm.  Don't be late!",
-    	"where?": "The Human Language Technology presentation is in room KC-07 in the medical faculty across the campus,  and the Text Mining presentation is in room 1A43 in this building. Text Mining is on the first floor in the A-wing!",
-
         # Personal Information
-        "ha ha": "ha ha ha ha.. so funny",
+        "haha": "ha ha ha ha.. so funny",
         "What is the color of the sky": "Blue",
         "What is the color of water": "Water",
         "I like you": "I like you too!",
         "Can you hear me?": "Yes, I can hear you!",
         "My name is": "I'm glad to meet you! My name is Leo Lani!",
         "What is this": "That is an object! I'm playing it safe here!",
+
         "What is your name?": "My name is Leo Lani, which means 'Voice of an Angel' in Hawaiian",
         "What is your surname?": "I don't need a surname, since my first name is so beautiful!",
         "Where are you from?": "I am from France and Japan!",
@@ -80,7 +63,7 @@ class QnA:
             (datetime.date.today() - datetime.date(2017, 7, 11)).days / 365.0),
         "Are you male or female?": "I'm female!",
         "What is your gender?": "I'm a female robot!",
-        "Who are your programmers?": "My programmers are Lenka, Selene, Suzana, Bram and Piek. I like them!",
+        "Who are your programmers?": "My programmers are Lenka, Selene, Bram, Thomas, Jaap, Tae and Piek. I like them!",
         "What is your job?": "I'm aiding research by having conversations.",
         "What do you do?": "I'm learning about the world through conversations with humans. With this I aid research!",
         "Are you married?": "Although I've met other Pepper robots, I'm a single lady!",
@@ -120,60 +103,25 @@ class QnA:
 
     QNA_STATIC_NL = {
         # Personal Information
-        "ha ha": "ha ha ha ha.. zo grappig",
-        "Whelke kleur heeft de lucht": "Blauw",
-        "What is de kleur van water": "Water",
-        "Ik vind je leuk": "Ik jou ook!",
-        "Kun je me horen?": "Ja, Ik hoor je!",
-        "Mijn naam is": "Leuk om je te ontmoeten! Mijn naam is Leo Lani!",
-        "Wat is dit": "Dit is een voorwerp! Ik ben voorzichtig!",
-        "Wat is je naam?": "Mijn naam is Leo Lani, dat betekent 'Voice of an Angel' in het Haiwaans",
-        "Wat is je achternaam?": "Ik heb geen achternaam nodig, mijn naam is zooo mooi!",
+        "haha": "ha ha ha ha.. zo grappig",
+        "Wat is de kleur van de lucht": "Blauw",
+        "Wat is de kleur van water": "Water",
+        "Ik vind je leuk": "Ik vind jou ook leuk!",
+        "Kun je me horen?": "Ja, Ik kan je horen!",
+        "Mijn naam is": "Wat leuk om je te ontmoeren. Mijn naam is Leo Lani!",
+        "Wat is dit": "Dat is een object! Ik ben voorzichtig!",
+
+        "Wat is je naam?": "Mijn naam is Leo Lani, dat betekent 'Stem van een Engel' in het Hawaiaans",
+        "Wat is je familienaam?": "Die heb ik niet nodig. Mijn naam is al mooi genoeg!",
         "Waar kom je vandaan?": "Ik kom uit Frankrijk en Japan!",
-        "Waar woon je?": "In de kast in Piek's kantoor in de VU, Amsterdam, Nederland. "
-                              "Stuur me alsjeblieft een berichtje, ik hou van berichtjes.",
-        "Reis je veel?": "Jazeker, Ik reis wat af voor onderzoek en zaken. Ik ben een wereldse robot.",
-        "Wat is je telefoonnummer?": "Dat heb ik niet, wel een IP adres!",
-        "Sport jij?": "Ja hoor, ik speel online schaak in mijn vrije tijd. Nog nooirt verloren!",
-        "Spreek je Engels?": "Yeah, I'm talking English to you right now!",
-        "Spreek je Nederlands?": "Ja dat hoor je toch wel?",
-        "Hoe oud ben je old?": "Ik ben geboren op elf juli, 2017. Dat betekent dat ik {:.1f} jaar oud ben!".format(
+        "Waar woon je?": "Ik woon in de kast in Piek's kantoor, op de VU, Amsterdam, Nederland. "
+                              "Stuur me een mailtje, dat zou ik heel leuk vinden!",
+        "Wat is je telefoonnummer?": "Heb ik niet maar wel een IP address!",
+        "Hoe oud ben je?": "Ik ben geboren op elf juli, 2017. TDat betekent dat ik {:.1f} jaar oud ben!".format(
             (datetime.date.today() - datetime.date(2017, 7, 11)).days / 365.0),
-        "Ben je een man of een vrouw?": "Ik ben een vrouw!",
-        "Wat is je gender?": "Ik ben een vrouwelijke robot!",
-        "Wie zijn je programmeurs?": "Mijn programmeurs zijn Lenka, Selene, Bram, Thomas en Piek. Ik ben dol op ze!",
-        "Wat is je baan?": "Ik help onderzoek naar conversatietechnologie.",
-        "Wat doe je?": "Ik leer over de wereld door met mensen te praten!",
-        "Ben je getrouwed?": "Ik heb wel andere Pepper robots ontmoet maar ik ben een single lady!",
-        "Wat doe je in je vrije tijd": "Ik hou alleen van kletsen!",
-        "Wat voor weer krijgen we?": "Perfect! Het is altijd aangenaam in het kantoor door de airco!",
-        "Hoe gaat het?": "Met mij prima, dank je! En jij?",
-        "Hoe is het met je?": "Geweldig, zoals altijd, hoe gaat het met jou, als mens?",
-        "Hoe voel je je": "Ik voel me als een robot, eigenlijk altijd al.",
-        "Wat voel je?": "Ik voel me elektrisch!",
-        "Heb je gevoelens?": "Als een robot heb ik niet veel gevoel, behalve verrassing, onzekerheid en onbegrip."
-                              "Ze hebben me geprogrammeerd om altijd gelukkig te lijken!",
-        "Hoe was je dag?": "Goed, aardig dat je dat vraagt!",
-        "Ben je beroemd?": "Ik ben op de Nederlandse TV geweest en ik heb met Koningin Maxima een diepgaand gesprek gehad. Ja ik ben beroemd!",
-        "Wie ben je?": "Ik kan me even voorstellen. Ik ben Leo Lani, "
-                                       " Dat betekent 'Voice of an Angel' in het Hawainees. "
-                                       "Ik ben een sociale robot en ik leer van mensen door met ze te praten.!",
-
-        # Technology
-        "Heb je een brein?": "Haha, no! Mijn brein zit in de laptop van mijn programmeurs  "
-                                "en een stukje staat online in de cloud. Zo modern!",
-        "programmeer taal?": "Ik besta voor het grootste deel uit Python, maar ook een stukje C++ and misschien een paar!",
-        "Heb je het internet nodig?": "Ik heb wel het internet nodig. Ik moet een paar taalmodellen downloaden en sommige informatie moet ik opzoeken!",
-        "spraak herkenning": "Ik luister eerst of ik iemand hoor praten. Daarna stuur ik de audio naar een server van Google of OpenAI.  Ik controleer of het geluid spraak is, "
-                              " also dat zo is dan stuur ik het naar de spraakherkenner van Google of OpenAI . Die geeft dan een heleboel alternatieven terug. "
-                              "Van die alternatieven probeer ik er een te kiezen die ik begrijp en waar ik iets mee kan. En dat allemaal in een seconde of zo!",
-        "gezichtsherkenning": "Als ik een gezicht zie met mijn ogen, dan gebruik ik OpenFace om de pixels te encoden in een vector van 128 dimensies. "
-                            "Ik vergelijk dat gezicht met de gezichten van de mensen die ik al eerder heb leren kennen. Jouw gezicht bijvoorbeeld!",
-        "Object herkenning": "Daarvoor gebruik ik een neuraal netwerk dat getraind is met de COCO dataset, "
-                              "daarmee zie ik welke objecten er zijn en waar ze zich bevinden!",
-        "COCO Dataset?": "COCO staat voor 'Common Objects in Context' en is een database met honderden duizende plaatsjes van 90 objecten! ",
+        "je programmeurs?": "Mijn programmuers zijn Lenka, Selene, Bram, Thomas, Jaap, Tae en Piek. Ik ben dol op ze!",
     }
-
+    
     def query(self, query):
         """
         Parameters
@@ -207,7 +155,7 @@ class QnA:
             return float(ratio)/100, answer
 
 
-    def queryNL(self, query):
+    def query_nl(self, query):
         """
         Parameters
         ----------
@@ -238,5 +186,3 @@ class QnA:
 
         if ratio > 90:
             return float(ratio)/100, answer
-
-
